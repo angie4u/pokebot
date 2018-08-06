8ab7e2ca4fa87643048c1b3084289baee003cddf
angie4u <eunk.angie@outlook.com>
2018-08-06 22:38:31 +0900

    fix #13 code refactor
    기존에는 한번에 20개 이상의 포켓몬 리스트를 호출하여 DB에 저장하는 것 불가했다. timeout 조정 및 SQL 쿼리 업데이트

--- database/saveinfo.js
+++ database/saveinfo.js
@@ -1,6 +1,50 @@
 require('dotenv-extended').load()
 
 const mysql = require('mysql')
+const utility = require('../utils/utility')
+
+const Pokedex = require('pokedex-promise-v2')
+const P = new Pokedex()
+var pokemonInfo = {}
+var params = []
+var sql = 'INSERT INTO pokeinfo (id, name_eng, name_kor, color, genra, imgUrl_large) VALUES ?'
+var startIndex = 430
+var endIndex = 470
+
+var promiseArray = []
+for (var i = startIndex; i < endIndex; i++) {
+  promiseArray.push(P.getPokemonSpeciesByName(i))
+}
+
+Promise.all(promiseArray).then(function (result) {
+  console.log(result)
+
+  for (var i in result) {
+    var response = result[i]
+    pokemonInfo.color = response.color.name
+    pokemonInfo.id = response.id
+    pokemonInfo.name_eng = response.name
+
+    var nameobj = response.names.filter(function (obj) { return obj.language.name === 'ko' })
+    pokemonInfo.name_kor = nameobj[0].name
+
+    var generaobj = response.genera.filter(function (obj) { return obj.language.name === 'ko' })
+    pokemonInfo.genera = generaobj[0].genus
+
+    pokemonInfo.imageUrl_large = utility.getLargeImgUrl(pokemonInfo.id)
+
+    params.push([pokemonInfo.id, pokemonInfo.name_eng, pokemonInfo.name_kor, pokemonInfo.color, pokemonInfo.genera, pokemonInfo.imageUrl_large])// 파라미터를 값들로 줌(배열로 생성)
+  }
+  return params
+}).then((params) => {
+  console.log(params)
+  conn.query(sql, [params], function (err, result) { // 쿼리문 두번째 인자로 파라미터로 전달함(값들을 치환시켜서 실행함. 보안과도 밀접한 관계가 있음(sql injection attack))
+    if (err) console.log(err)
+  })
+}).catch(function (error) {
+  console.log(error)  // logs 'failure.'
+})
+
 var conn = mysql.createConnection({
   host: process.env.MYSQL_HOST,
   port: process.env.MYSQL_PORT,
@@ -8,58 +52,3 @@ var conn = mysql.createConnection({
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE
 })
-
-const Pokedex = require('pokedex-promise-v2')
-const P = new Pokedex()
-var pokemonInfo = {}
-// var i = '1'
-function startRequest (i) {
-// for (var i = 27; i < 49; i++) {
-
-  P.getPokemonSpeciesByName(i) // with Promise
-    .then(function (response) {
-      console.log('response: ' + response)
-
-      pokemonInfo.color = response.color.name
-      pokemonInfo.id = response.id
-      pokemonInfo.name_eng = response.name
-    // pokemonInfo.habitat = response.habitat.name
-
-      var nameobj = response.names.filter(function (obj) { return obj.language.name === 'ko' })
-      pokemonInfo.name_kor = nameobj[0].name
-
-      var generaobj = response.genera.filter(function (obj) { return obj.language.name === 'ko' })
-      pokemonInfo.genera = generaobj[0].genus
-        // console.log(pokemonInfo)
-      return pokemonInfo
-    })
-    .then(function (pokemonInfo) {
-      pokemonInfo.imageUrl_large = getLargeImgUrl(pokemonInfo.id)
-    // const message = CardFactory.adaptiveCard(infoCard(pokemonInfo.imageUrl_large, pokemonInfo.name_kor, pokemonInfo.id, pokemonInfo.name_eng, pokemonInfo.genera, pokemonInfo.habitat, pokemonInfo.color))
-
-      var sql = 'INSERT INTO pokeinfo (id, name_eng, name_kor, color, genra, imgUrl_large) VALUES(?,?,?,?,?,?)'
-      var params = [pokemonInfo.id, pokemonInfo.name_eng, pokemonInfo.name_kor, pokemonInfo.color, pokemonInfo.genera, pokemonInfo.imageUrl_large]// 파라미터를 값들로 줌(배열로 생성)
-      conn.query(sql, params, function (err, rows, fields) { // 쿼리문 두번째 인자로 파라미터로 전달함(값들을 치환시켜서 실행함. 보안과도 밀접한 관계가 있음(sql injection attack))
-        if (err) console.log(err)
-        // console.log(rows.insertId)
-      })
-    // }
-    })
-    .catch(function (error) {
-      console.log('There was an ERROR: ', error)
-    })
-// }
-}
-
-function getLargeImgUrl (id) {
-  var formattedNumber
-  if (id < 10) {
-    formattedNumber = ('00' + id).slice(-3)
-  } else if (id < 100) {
-    formattedNumber = ('0' + id).slice(-3)
-  } else {
-    formattedNumber = id
-  }
-  var url = 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/' + formattedNumber + '.png'
-  return url
-}
